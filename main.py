2cbb2d2252992ebd076e2bd9b80055df444c480c
Ru Han / Dave <63888029+ruhan-dave@users.noreply.github.com>
2024-01-11 02:34:34 -0800

    Update main.py

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ def my_form_post():
     with open("final.pkl", 'rb') as f:
         model = pickle.load(f)
     answer = int(model.predict(text)[0])
-    neg_face = emoji.emojize(":grimacing_face:")
-    pos_face = emoji.emojize(":slightly_smiling_face:")
+    neg_face = emoji.emojize(':grimacing_face:')
+    pos_face = emoji.emojize(':slightly_smiling_face:')
     if answer == 0:
         label = f"This review is negative {neg_face}"
     if answer == 1:
